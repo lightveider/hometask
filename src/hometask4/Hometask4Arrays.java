19249312cdfb62831b298f482a58368f09a94791
Dmitry Mozhaev <lightveider@gmail.com>
2021-03-23 20:12:05 +0300

    hometask4 arrays start

---
+++ src/hometask4/Hometask4Arrays.java
@@ -0,0 +1,14 @@
+package hometask4;
+
+public class Hometask4Arrays {
+    public static void main (String[] args){
+
+        int[] arr = new int[20];
+        int[] arr2 = new int [20];
+
+        
+
+
+
+    }
+}
