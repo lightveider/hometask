9fbdb99de3e5082b2614f90f61bd6dcf4bba08b1
Dmitry Mozhaev <lightveider@gmail.com>
2021-03-24 16:52:35 +0300

    Hometask 4 Arrays

--- src/hometask4/Hometask4Arrays.java
+++ src/hometask4/Hometask4Arrays.java
@@ -1,14 +1,57 @@
 package hometask4;
 
+import java.util.Arrays;
+
 public class Hometask4Arrays {
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
-        int[] arr = new int[20];
-        int[] arr2 = new int [20];
+        //Задание1
+        //
+    /*    double[] arr = new double[20];
+        double min = Double.MIN_VALUE;
+        double max = Double.MAX_VALUE;
+        double exchange;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Math.random() * 497 + 3;
+            if (arr[i] < max) max = arr[i]; // поиск минимального
+            if (arr[i] > min) min = arr[i]; // поиск максимального
 
-        
+
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println(min);
+        System.out.println(max);
+
+*/
+        /*int counter = 0;
+        int counterNeChet = 0;
+        int array[] = {78, 32, 76, 100, 0, 6301, 331, 77, 24, 2, 90};
 
 
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                counter++;
+            }
 
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            int
+
+        }
+        System.out.println(counter);
+*/
+
+        int abc[] = new int[10];
+        int cba[] = new int[10];
+        for (int i = 0, j = 9; i < abc.length; i++,j--) {
+            abc[i] = i * 2 + 2;
+            cba[j] = abc[i];
+
+            }
+
+
+        System.out.println(Arrays.toString(abc));
+        System.out.println(Arrays.toString(cba));
     }
 }
